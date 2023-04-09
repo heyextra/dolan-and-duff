10104b72c2efb9fb30980efd4581f880096d4055
David Simonson <90648940+heyextra@users.noreply.github.com>
2023-04-09 18:57:26 -0400

    Update main.js
    faster

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,27 +1,27 @@
 
 
 const enterAnimation = (container) => {
-  return gsap.from(container, {autoAlpha: 0, clearProps:"all", duration: .8, ease: "none"})
+  return gsap.from(container, {autoAlpha: 0, clearProps:"all", duration: .6, ease: "none"})
 }
 const leaveAnimation = (container) => {
-  return gsap.to(container, {autoAlpha: 0, clearProps:"all",  duration: .8, ease: "none" })
+  return gsap.to(container, {autoAlpha: 0, clearProps:"all",  duration: .6, ease: "none" })
 }
 const buttonLeaveAnimation = () => {
   var tl = gsap.timeline()
-  tl.to("button", {transform: "translate(0px, 0px)", duration: 1})
+  tl.to("button", {transform: "translate(0px, 0px)", duration: .8})
   
 }
 const buttonEnterAnimation = () => {
- gsap.to("button", {transform: "translate(0px, -300px)", opacity: 1, delay:1.3, duration: 1})
+ gsap.to("button", {transform: "translate(0px, -300px)", opacity: 1, delay:1.1, duration: .8})
 }
 
 const pageAnimation = () => {
 
 var tl = gsap.timeline({defaults:{duration: 1}})
 tl.from("main", { y:"100px"} )
-.from("#heading", {opacity: 0, y:"-100px"},"-=.7")
-.from(".desc", {opacity:0},"-=.8")
-.from("aside", {clipPath: "polygon(50% 100%, 100% 50%, 50% 100%, 0% 50%)"},"-=1.5")
+.from("#heading", {opacity: 0, y:"-100px"},"-=.5")
+.from(".desc", {opacity:0},"-=.6")
+.from("aside", {clipPath: "polygon(50% 100%, 100% 50%, 50% 100%, 0% 50%)"},"-=1")
 }
 
 
@@ -34,7 +34,7 @@ const pageLeaveAnimation = () => {
 
     var tl = gsap.timeline({defaults:{duration: 1}})
     tl.from("#core", {opacity:0, y:"100px"} )
-    .from(".service", {opacity: 0, y:"-20px", stagger: 0.1, ease: "power2.in" },"-=.7")
+    .from(".service", {opacity: 0, y:"-20px", stagger: 0.1, ease: "power2.in" },"-=.5")
     }
 
 
